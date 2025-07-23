e38300a1b64cbf40fad0c3cdebcecd9f19cae994
Abimael <abimael.spdev@gmail.com>
2025-07-22 20:07:02 -0400

    feat: ensure password and confirmPassword are the same on signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -29,19 +29,27 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { api } from '@/lib/axios';
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, 'Nome é obrigatório'),
-  lastName: z.string().trim().min(1, 'Sobrenome é obrigatório'),
-  email: z.string().trim().email('Email é inválido'),
-  password: z.string().trim().min(6, 'Senha deve ter pelo menos 6 caracteres'),
-  confirmPassword: z
-    .string()
-    .trim()
-    .min(6, 'Confirmação de senha é obrigatória'),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você deve aceitar os termos de uso',
-  }),
-});
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, 'Nome é obrigatório'),
+    lastName: z.string().trim().min(1, 'Sobrenome é obrigatório'),
+    email: z.string().trim().email('Email é inválido'),
+    password: z
+      .string()
+      .trim()
+      .min(6, 'Senha deve ter pelo menos 6 caracteres'),
+    confirmPassword: z
+      .string()
+      .trim()
+      .min(6, 'Confirmação de senha é obrigatória'),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você deve aceitar os termos de uso',
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem.',
+    path: ['confirmPassword'],
+  });
 
 const SignupPage = () => {
   const [user, setUser] = useState(null);
